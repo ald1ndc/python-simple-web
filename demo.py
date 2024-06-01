df46fcd30e28d28286352f062eb956ad32bbe675
ald1ndc <aldindc@yahoo.com>
2024-06-01 13:11:22 +0800

    Added demo.py and requirements.tx

---
+++ demo.py
@@ -0,0 +1,17 @@
+#If else 
+a = 10
+if a > 10:
+  print("This is a big number")
+else:
+  print("This is a small number")
+
+#Loops
+a = 0
+while a < 10:
+    print(f"a: {a}")
+    a = a + 1
+#Functions
+def hello(name):
+   print(f"Hello World, {name}")
+
+hello("Aladdin")
